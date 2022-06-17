44e92c3266a938bbc60a572207c0660ff0d0c38d
Carlos Puente <carlospuenteg@gmail.com>
2022-06-17 23:41:58 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,118 +1,212 @@
 import random
 import hashlib
-from colorama import init, Fore, Back
-init(autoreset=True)
-#-----------------------------------------------------------------------------
-# https://www.geeksforgeeks.org/python-import-module-from-different-directory/
-import sys
-sys.path.insert(0, 'mods/')
-#-----------------------------------------------------------------------------
-from hash2h import h2h
-from txtHex import t2h, h2t, txtB
-from enc1 import enc, dec
+from colorama import init, Fore, Back; init(autoreset=True)
 
-##################################################################################################################################
+############################################################################
+BASE = "0123456789abcdefghijklmnopqrstuvwxyz .,'"; 
+BASE_LEN = len(BASE)
+HEX_SYMB = "0123456789abcdef"
+MAX_LEN = 94
+TXT_MAX_LEN = 126
+HASH_ITERS = 100
+ITERS_TOADD = 10
+ENC_CODE_POS = 0
+TEXT_LEN_POS = [1,2]
+ENC_LIST_LEN = 256
 
-# https://newbedev.com/build-python-script-to-executable-file-for-windows-mac-os-linux
-# pyinstaller --onefile test.py
+##########################################################################################################
+# Create a new hash from a hash
+def h2h(hash, iters):
+    for _ in range(iters):
+        hash = hashlib.sha512(hash.encode()).hexdigest()
+    return hash
 
-#-----------------------------------------------------------------------------
+# Encrypt a text with an encryption code
+def enc(txt,encryption_code):
+    encTxt = ""
+    for x in txt:
+        encTxt += encryption_code[int(x,16)]  # The symbol in the equivalent position
+    return encTxt
 
-symb = "0123456789abcdef"
-maxLen = 94
-textMLen = 126
+# Decrypt an encrypted text with an encryption code
+def dec(txt,encryption):
+    decTxt = ""
+    for x in txt:
+        decTxt += HEX_SYMB[encryption.index(x)]  # The symbol in the equivalent position
+    return decTxt
 
-##################################################################################################################################
+# Convert from text to hexadecimal
+def t2h(txt):
+    num = 0
+    for x in range(0,len(txt)):
+        num += ( BASE.index(txt[x]) * BASE_LEN**(len(txt)-x-1) )
 
-def encrypt(txt,pwd):
-    if not all(c in txtB for c in txt) or len(txt)>maxLen:
-        return -1
-    encList = [None] * 256
-    #-----------------------------------------------------------------------------------------------------------------------------
-    hashword = hashlib.sha512(pwd.encode()).hexdigest()
+    return format(num, 'x')
 
-    iters = 100
+# Convert hexadecimal to text
+def h2t(hex):
+    num = 0
+    for x in range(0,len(hex)):
+        num += ( HEX_SYMB.index(hex[x]) * 16**(len(hex)-x-1) )
+
+    d = []
+    while num:
+        d.append(BASE[num % BASE_LEN])
+        num //= BASE_LEN
+
+    return "".join(d[::-1])
+
+##########################################################################################################
+def encrypt(txt, pwd):
+    # If the text has any strange symbol or its length is more than the max, return -1
+    if not all(c in BASE for c in txt) or len(txt) > MAX_LEN: return -1
+
+    # Encode the password to sha512 and get the hash (length = 128)
+    hash = hashlib.sha512(pwd.encode()).hexdigest()
+
+    # Initial number of iterations for the hash
+    iters = HASH_ITERS
+
+    # Combine 3 consecutive hashes, created from the initial hash
+    hash = h2h(hash, iters)
+    
+    # Add ITERS_TOADD to the iterations count
+    iters += ITERS_TOADD
+
     while True:
-        hashword = h2h(hashword,iters)+h2h(hashword,iters+1)+h2h(hashword,iters+2)
-        hashwLi = [int(hashword[i:i+2],16) for i in range(0, len(hashword), 2)]
-        hashwLi = list(dict.fromkeys(hashwLi))
-        if len(hashwLi)>=(textMLen+2):
+        # Convert each pair of hex digits from the hash to an integer
+        pwd_indexes = [int(hash[i : i+2], 16) for i in range(0, len(hash), 2)]
+
+        # Remove the duplicates from the list
+        pwd_indexes = list(dict.fromkeys(pwd_indexes))
+
+        # If the list has more than the required length (TXT_MAX_LEN + ENC_CODE_POS + TEXT_LEN_POS), break (end the loop)
+        if len(pwd_indexes) >= (TXT_MAX_LEN + 3): 
             break
-        iters += 10
-    #-----------------------------------------------------------------------------------------------------------------------------
-    encryption = ""
 
-    iters = 200
-    hashword = h2h(hashword,iters)+h2h(hashword,iters+1)+h2h(hashword,iters+2)
-    while len(encryption)<16:
-        if (hashword[hashwLi[50]] not in encryption):
-            encryption += hashword[hashwLi[50]]
-        else: 
-            iters += 10
-            hashword = h2h(hashword,iters)+h2h(hashword,iters+1)+h2h(hashword,iters+2)
-    #-----------------------------------------------------------------------------------------------------------------------------
-    txtEnc = enc(t2h(txt),encryption)
-    txtPos = hashwLi[:textMLen]
+        # If the list doesn't have enough indexes, add other hash to the hash
+        hash += h2h(hash, iters)
 
-    txtLen = len(txtEnc)
-    txtLenEnc = enc(format(txtLen, 'x').rjust(2,"0"),encryption)
-    txtLenPos = hashwLi[textMLen:textMLen+2]
-    #-----------------------------------------------------------------------------------------------------------------------------
-    for x in range(0,256):
-        encList[x] = random.choice(symb)
-    for x in range(0,len(txtEnc)):
-        encList[txtPos[x]] = txtEnc[x]
-    for x in range(0,len(txtLenPos)):
-        encList[txtLenPos[x]] = txtLenEnc[x]
+        # Increase the iterations count by ITERS_TOADD
+        iters += ITERS_TOADD
 
-    encText = "".join(encList)
-    if (decrypt(encText,pwd) == txt):
-        return encText
+    # Create a new hash to use to obtain the symbols used in the encryption and their order
+    hash = h2h(hash, iters)
+
+    # Symbols used in the encryption and their order
+    encryption_code = ""
+
+    # While the encryption string doesn't have 16 symbols
+    while len(encryption_code) < 16:
+        # If the symbol of the encr_hash in the position contained in pwd_indexes[ENC_CODE_POS] is not in the encryption string, add it to the encryption code
+        if (hash[ENC_CODE_POS] not in encryption_code):
+            encryption_code += hash[ENC_CODE_POS]
+        # Create a new hash
+        iters += ITERS_TOADD
+        hash = h2h(hash,iters)
+
+    # Encrypt the text (converted to hexadecimal) with the encryption code
+    txt_enc = enc(t2h(txt), encryption_code)
+
+    # Indexes for each character in the encrypted text
+    txt_indexes = pwd_indexes[3:]
+
+    # Encrypt the length of the text (converted to hexadecimal - in string) and adding a 0 to the left if the length is less than 2 (f -> 0f)
+    txt_len_enc = enc(format(len(txt_enc), 'x').rjust(2,"0"), encryption_code)
+
+    # Save the two indexes where the text length will be saved
+    txt_len_indexes = [pwd_indexes[TEXT_LEN_POS[0]] , pwd_indexes[TEXT_LEN_POS[1]]]
+
+    # Fill the encrypted text with random characters
+    toret = [random.choice(HEX_SYMB) for _ in range(ENC_LIST_LEN)]
+
+    # Place each character in the encrypted text in its position from txt_indexes
+    for x in range(len(txt_enc)):
+        toret[txt_indexes[x]] = txt_enc[x]
+        
+    # Place the length of the text in the two indexes from txt_len_indexes
+    for x in range(0,len(txt_len_indexes)):
+        toret[txt_len_indexes[x]] = txt_len_enc[x]
+
+    # Join the toret list
+    toret = "".join(toret)
+
+    # If the text can be decrypted with the same password, return the result.
+    if (decrypt(toret, pwd) == txt):
+        return toret
+    # Else, something went wrong
     else:
         return -1
 
-##################################################################################################################################
+##########################################################################################################
 
 def decrypt(txt,pwd):
-    if not all(c in symb for c in txt) or len(txt)>257:
-        return -1
-    #-----------------------------------------------------------------------------------------------------------------------------
-    hashword = hashlib.sha512(pwd.encode()).hexdigest()
+    # If the text has any strange symbol or its length is more than the max, return -1
+    if not all(c in BASE for c in txt) or len(txt) > ENC_LIST_LEN: return -1
 
-    iters = 100
+    # Encode the password to sha512 and get the hash (length = 128)
+    hash = hashlib.sha512(pwd.encode()).hexdigest()
+
+    # Initial number of iterations for the hash
+    iters = HASH_ITERS
+
+    # Combine 3 consecutive hashes, created from the initial hash
+    hash = h2h(hash, iters)
+    
+    # Add ITERS_TOADD to the iterations count
+    iters += ITERS_TOADD
+
     while True:
-        hashword = h2h(hashword,iters)+h2h(hashword,iters+1)+h2h(hashword,iters+2)
-        hashwLi = [int(hashword[i:i+2],16) for i in range(0, len(hashword), 2)]
-        hashwLi = list(dict.fromkeys(hashwLi))
-        if len(hashwLi)>=(textMLen+2):
+        # Convert each pair of hex digits from the hash to an integer
+        pwd_indexes = [int(hash[i : i+2], 16) for i in range(0, len(hash), 2)]
+
+        # Remove the duplicates from the list
+        pwd_indexes = list(dict.fromkeys(pwd_indexes))
+
+        # If the list has more than the required length (TXT_MAX_LEN + ENC_CODE_POS + TEXT_LEN_POS), break (end the loop)
+        if len(pwd_indexes) >= (TXT_MAX_LEN + 3): 
             break
-        iters += 10
-    #-----------------------------------------------------------------------------------------------------------------------------
-    encryption = ""
 
-    iters = 200
-    hashword = h2h(hashword,iters)+h2h(hashword,iters+1)+h2h(hashword,iters+2)
-    while len(encryption)<16:
-        if (hashword[hashwLi[50]] not in encryption):
-            encryption += hashword[hashwLi[50]]
-        else: 
-            iters += 10
-            hashword = h2h(hashword,iters)+h2h(hashword,iters+1)+h2h(hashword,iters+2)
-    #-----------------------------------------------------------------------------------------------------------------------------
-    txtPos = hashwLi[:textMLen]
-    txtLenPos = hashwLi[textMLen:textMLen+2]
-    txtLen = int(dec(txt[txtLenPos[0]]+txt[txtLenPos[1]],encryption),16)
-    fiText = ""
-    for x in range(0,txtLen):
-        fiText += txt[txtPos[x]]
-    fiText = h2t(dec(fiText,encryption))
+        # If the list doesn't have enough indexes, add other hash to the hash
+        hash += h2h(hash, iters)
 
-    return fiText
+        # Increase the iterations count by ITERS_TOADD
+        iters += ITERS_TOADD
 
-##################################################################################################################################
+    # Create a new hash to use to obtain the symbols used in the encryption and their order
+    hash = h2h(hash, iters)
 
+    # Symbols used in the encryption and their order
+    encryption_code = ""
+
+    # While the encryption string doesn't have 16 symbols
+    while len(encryption_code) < 16:
+        # If the symbol of the encr_hash in the position contained in pwd_indexes[ENC_CODE_POS] is not in the encryption string, add it to the encryption code
+        if (hash[ENC_CODE_POS] not in encryption_code):
+            encryption_code += hash[ENC_CODE_POS]
+        # Create a new hash
+        iters += ITERS_TOADD
+        hash = h2h(hash,iters)
+
+    # Indexes for each character in the encrypted text
+    txt_indexes = pwd_indexes[3:]
+
+    # Save the two indexes where the text length will be saved
+    txt_len_indexes = [pwd_indexes[TEXT_LEN_POS[0]] , pwd_indexes[TEXT_LEN_POS[1]]]
+
+    # Get the text length from the text in the txt_len_indexes, decrypt it and convert it to integer
+    txt_len = int(dec(txt[txt_len_indexes[0]]+txt[txt_len_indexes[1]], encryption_code), 16)
+
+    toret = ""
+    for x in range(txt_len):
+        toret += txt[txt_indexes[x]]
+    toret = h2t(dec(toret, encryption_code))
+
+    return toret
+
+##########################################################################################################
 def menu():
-    print("\nThis algorithm helps you encrypt a text with a maximum length of " + str(maxLen) + ", written with 40 different characters and symbols")
+    print("\nThis algorithm helps you encrypt a text with a maximum length of " + str(MAX_LEN) + ", written with 40 different characters and symbols")
 
     while True:
         print("  0. EXIT: ")
@@ -126,26 +220,25 @@ def menu():
         elif opt == "1":
             txt = input("Type the text to encrypt: ")
             pwd = input("Type a password: ")
-            encText = encrypt(txt,pwd)
-            if encText != -1:
-                print("\nEncrypted text: " + Fore.GREEN+encText+"\n")
+            enc_txt = encrypt(txt,pwd)
+            if enc_txt != -1:
+                print("\nEncrypted text: " + Fore.GREEN+enc_txt+"\n")
             else:
                 print("\n"+Fore.RED+"Invalid text"+"\n")
         elif opt == "2":
             txt = input("Type the text to decrypt: ")
             pwd = input("Type the password: ")
-            decText = decrypt(txt,pwd)
-            if decText != -1:
-                print("\nDecrypted text: " + Fore.LIGHTCYAN_EX+decText+"\n")
+            dec_txt = decrypt(txt,pwd)
+            if dec_txt != -1:
+                print("\nDecrypted text: " + Fore.LIGHTCYAN_EX+dec_txt+"\n")
             else:
                 print("\n"+Fore.RED+"Invalid text"+"\n")
         elif opt == "s":
             baseList = ""
-            for x in txtB:
+            for x in BASE:
                 baseList += Back.BLUE+" "+x+" "
                 baseList += Back.RESET+" "
             print("\n"+baseList+"\n")
 
 ##################################################################################################################################
-
 menu()
