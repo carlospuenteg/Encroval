3747b73b3446e57864d1d9876c91fb032f0ae786
Carlos Puente <carlospuenteg@gmail.com>
2022-10-01 15:44:09 +0200

    Delete classes directory

--- classes/Text.py
+++
@@ -1,9 +0,0 @@
-from colorama import Fore, init; init()
-
-class Text:
-    def __init__(self, text:str, color:str):
-        self.text = text
-        self.color = color
-
-    def __str__(self) -> str:
-        return f"{self.color}{self.text}{Fore.RESET}"
